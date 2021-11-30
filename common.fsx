48ad943572b8b47f6223260becc76cb1c791a22d
Alexey Mazharenko <mazharenko.a@yandex.ru>
2021-11-30 16:09:32 +0500

    initial commit

---
+++ common.fsx
@@ -0,0 +1,11 @@
+[<AutoOpen>]
+module common
+
+module String =
+    let fromArray (ar:char[]) = 
+        new string(ar)
+        
+module Array2D =
+    let toArray (source: 'T[,]) =
+        source |> Seq.cast<'T> |> Seq.toArray
+    
